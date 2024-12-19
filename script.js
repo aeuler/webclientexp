6a77eb421c20fddae9498f3ec547e8fcfca3d53a
Andrew Euler <107337508+aeuler@users.noreply.github.com>
2024-12-19 13:27:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function addNote() {
+    const noteInput = document.getElementById("noteInput");
+    const notes = document.getElementById("notes");
+    const inputValue = noteInput.value.trim();
+
+    if (inputValue !== "") {
+        const note = document.createElement("div");
+        note.className = "note";
+        note.textContent = inputValue;
+
+        if (inputValue.toLowerCase() === "/help") {
+            const defaultResponse = document.createElement("div");
+            defaultResponse.className = "note default-response";
+            defaultResponse.textContent = "S3rv3r_S1d3_V@lid@ti0n_1s_B3st!";
+            notes.appendChild(defaultResponse);
+        }
+
+        notes.appendChild(note);
+        noteInput.value = "";
+    } else {
+        alert("Please enter a note.");
+    }
+}
